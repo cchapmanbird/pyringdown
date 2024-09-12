9522fd70cf97d85b9632567ea961e7fc20777c5f
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2024-09-12 15:30:56 +0100

    add group plotting function

---
+++ src/pyringdown/plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+import os
+
+def parameter_group_plot(filenames, parameter, mapping_function=None, outname=None, xvalues=None, xlabel=None, parent_dir=None):
+    if xvalues is None:
+        xvalues = np.arange(len(filenames))
+    if parent_dir is None:
+        parent_dir = ''
+    if xlabel is None:
+        xlabel = ''
+
+    to_plot = []
+
+    for fn in filenames:
+        with h5py.File(os.path.join(parent_dir, fn)) as f:
+            if mapping_function is None:
+                to_plot.append(f['posterior_samples'][parameter][:].mean())
+            else:
+                samples = f['posterior_samples']
+                remapped = mapping_function(samples)
+                to_plot.append(remapped.mean())
+
+    fig, ax = plt.subplots()
+    ax.plot(xvalues, to_plot)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(parameter)
+
+    if outname is None:
+        return fig, ax
+    else:
+        fig.savefig(outname, bbox_inches="tight")
+
